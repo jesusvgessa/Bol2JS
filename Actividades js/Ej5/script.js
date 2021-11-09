502b77baa4bec80c3ec9c167fe663ed95dc9393b
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-09 12:05:19 +0100

    cambio ej5

--- Actividades js/Ej5/script.js
+++ Actividades js/Ej5/script.js
@@ -2,28 +2,52 @@ function realizarOperacion() {
     //Entorno:
     var sOpe = document.getElementById("ope").value;
     var sMensaje = "";
+    var caracteres = /[*+/-]/i;
 
     //Algoritmo:
-    if(sOpe.length > 2){
-        while (sOpe.length > 2){
-            console.log(sOpe.indexOf('*+/-'));
-            var iNum1 = Number(sOpe.substr(0,sOpe.indexOf(['*+/-'])));
-            console.log(iNum1);
-            console.log(sOpe);
-            var sOperacion = sOpe.substr(0,1);
-            console.log(sOperacion);
-            console.log(sOpe);
-            var iNum2 = Number(sOpe.substr(0,sOpe.indexOf(['*+/-'])));
-            console.log(iNum2);
-            console.log(sOpe);
+    if (sOpe.length > 2) {
+        while (sOpe.length > 2) {
+            //Saco el primer numero y lo quito de la operacion total
+            var iNum1 = sOpe.substr(0, sOpe.indexOf(sOpe.match(caracteres)));
+            sOpe = sOpe.substr(sOpe.indexOf(sOpe.match(caracteres)), sOpe.length - 1);
 
-            sOpe = iNum1+sOperacion+iNum2+sOpe;
-            console.log(sOpe);
-        }//Fin Mientras
-        sMensaje = "El resultado es: "+sOpe;
-    }else{
+            //Hago lo mismo con el operador
+            var sOperador = sOpe.substr(0, 1);
+            sOpe = sOpe.substr(1, sOpe.length - 1);
+
+            if (sOpe.length > 2) {
+                var iNum2 = sOpe.substr(0, sOpe.indexOf(sOperador));
+                sOpe = sOpe.substr(1, sOpe.length - 1);
+            } else {
+                //Si tiene menos de 2, es porque queda solo un numero.
+                var iNum2 = sOpe;
+                sOpe = "";
+            } //Fin Si
+
+            var iResultado = 0;
+            switch (sOperador) {
+                case '+':
+                    iResultado = Number(iNum1) + Number(iNum2);
+                    break;
+                case '*':
+                    iResultado = Number(iNum1) * Number(iNum2);
+                    break;
+                case '/':
+                    iResultado = Number(iNum1) / Number(iNum2);
+                    break;
+                case '-':
+                    iResultado = Number(iNum1) - Number(iNum2);
+                    break;
+                default:
+                    sOpe = "";
+            } //Fin Segun Sea
+
+            sOpe = iResultado + sOpe;
+        } //Fin Mientras
+        sMensaje = "El resultado es: " + sOpe;
+    } else {
         sMensaje = "Debe introducir una operacion";
-    }//Fin Si
-    
+    } //Fin Si
+
     document.getElementById("resultado").innerHTML = sMensaje;
 } //Fin Funcion
