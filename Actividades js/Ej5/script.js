ae295eef4ec7ac63a8c736b7391785ad125cc514
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-10 20:36:04 +0100

    ej hechos

--- Actividades js/Ej5/script.js
+++ Actividades js/Ej5/script.js
@@ -5,8 +5,8 @@ function realizarOperacion() {
     var caracteres = /[*+/-]/i;
 
     //Algoritmo:
-    if (sOpe.length > 2) {
-        while (sOpe.length > 2) {
+    if (sOpe.match(caracteres) != null) {
+        while (sOpe.match(caracteres) != null) {
             //Saco el primer numero y lo quito de la operacion total
             var iNum1 = sOpe.substr(0, sOpe.indexOf(sOpe.match(caracteres)));
             sOpe = sOpe.substr(sOpe.indexOf(sOpe.match(caracteres)), sOpe.length - 1);
@@ -15,8 +15,9 @@ function realizarOperacion() {
             var sOperador = sOpe.substr(0, 1);
             sOpe = sOpe.substr(1, sOpe.length - 1);
 
-            if (sOpe.length > 2) {
-                var iNum2 = sOpe.substr(0, sOpe.indexOf(sOperador));
+            if (sOpe.match(caracteres) != null) {
+                var iNum2 = sOpe.substr(0, sOpe.indexOf(sOpe.match(caracteres)));
+                console.log(iNum2)
                 sOpe = sOpe.substr(1, sOpe.length - 1);
             } else {
                 //Si tiene menos de 2, es porque queda solo un numero.
@@ -33,7 +34,7 @@ function realizarOperacion() {
                     iResultado = Number(iNum1) * Number(iNum2);
                     break;
                 case '/':
-                    iResultado = Number(iNum1) / Number(iNum2);
+                    iResultado = Math.trunc(Number(iNum1) / Number(iNum2));
                     break;
                 case '-':
                     iResultado = Number(iNum1) - Number(iNum2);
