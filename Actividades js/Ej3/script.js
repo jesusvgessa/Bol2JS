4cdf94ac1fb571fa22b78ec4ea0d1878d0f14c09
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-10 20:00:54 +0100

    cambios

--- Actividades js/Ej3/script.js
+++ Actividades js/Ej3/script.js
@@ -1,10 +1,31 @@
 function pasarNumero() {
     //Entorno:
     var iNum = Number(document.getElementById("num").value);
+    var sCadena = "";
+    var iVeces = iNum;
+    var iCont = 1;
 
     //Algoritmo:
-    while (i <= ) {
-
+    while (iVeces>=0) {
+        for(var i=1;i<=iNum;i++){
+            do{
+               sCadena = sCadena + i + " ";
+               iVeces--;
+            }while(iVeces>0);
+            sCadena = sCadena+"<br>";
+            iVeces = iNum - i;
+        }//Fin Para
+        iVeces = iNum - iCont;
+        for(var i=iNum;i>=1;i--){
+            do{
+                sCadena = sCadena + i + " ";
+                iVeces++;
+            }while(iVeces<iNum);
+            sCadena = sCadena+"<br>";
+            iCont++;
+            iVeces = iNum - iCont;
+        }//Fin Para
+        iVeces--;
     } //Fin Mientras
-
+    document.getElementById("resultado").innerHTML = sCadena;
 } //Fin Funcion
