ca3622d39394a64a453e878a061c520072586781
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-11 13:04:25 +0100

    cambios

--- Actividades js/Ej4/script.js
+++ Actividades js/Ej4/script.js
@@ -9,47 +9,47 @@ function validarContrasena() {
 
     //Algoritmo:
     //Recorro la contraseña entera y utilizo un contador para cada tipo de caracter requerido
-    if(sContra.length>=10){
+    if (sContra.length >= 10) {
 
-        for(var i=0;i<sContra.length;i++){
-            if ( (sContra.charCodeAt(i)>=33 && sContra.charCodeAt(i)<=96) || (sContra.charCodeAt(i)>=123 && sContra.charCodeAt(i)<=126) ){
-                if (sContra.charCodeAt(i)>=48 && sContra.charCodeAt(i)<=57){
+        for (var i = 0; i < sContra.length; i++) {
+            if ((sContra.charCodeAt(i) >= 33 && sContra.charCodeAt(i) <= 96) || (sContra.charCodeAt(i) >= 123 && sContra.charCodeAt(i) <= 126)) {
+                if (sContra.charCodeAt(i) >= 48 && sContra.charCodeAt(i) <= 57) {
                     //Numero
                     iNum++;
-                }else if(sContra.charCodeAt(i)>=65 && sContra.charCodeAt(i)<=90){
+                } else if (sContra.charCodeAt(i) >= 65 && sContra.charCodeAt(i) <= 90) {
                     //mayus
                     iMayus++;
-                }else{
+                } else {
                     //caracter especial
                     iCar++;
-                }//Fin Si
-            }else{
+                } //Fin Si
+            } else {
                 //minus
                 iMinus++;
-            }//Fin Si
-        }//Fin Para
+            } //Fin Si
+        } //Fin Para
 
-        if(iCar>=0.1*sContra.length && iNum>=0.2*sContra.length && iMinus>=(1/3)*(iMayus+iMinus) && iMayus>=(1/3)*(iMayus+iMinus)){
+        if (iCar >= 0.1 * sContra.length && iNum >= 0.2 * sContra.length && iMinus >= (1 / 3) * (iMayus + iMinus) && iMayus >= (1 / 3) * (iMayus + iMinus)) {
             sMensaje = "Su contrasena es valida, y se considera como fuerte.";
-        }else{
+        } else {
             sMensaje = "Su contrasena es valida, pero para considerarse como fuerte: <br>";
 
-            if(iCar<0.1*sContra.length){
-                sMensaje = sMensaje + "debe tener al menos el 10% de caracteres especiales.<br>";
-            }//Fin Si
+            if (iCar < 0.1 * sContra.length) {
+                sMensaje = sMensaje + "- Debe tener al menos el 10% de caracteres especiales.<br>";
+            } //Fin Si
 
-            if(iNum<0.2*sContra.length){
-                sMensaje = sMensaje + "debe tener al menos el 20% numeros.<br>";
-            }//Fin Si
+            if (iNum < 0.2 * sContra.length) {
+                sMensaje = sMensaje + "- Debe tener al menos el 20% numeros.<br>";
+            } //Fin Si
 
-            if(iMinus>=(1/3)*(iMayus+iMinus) || iMayus>=(1/3)*(iMayus+iMinus)){
-                sMensaje = sMensaje + "debe tener al menos 1/3 de los caracteres diferentes(mayus/minus).<br>";
-            }//Fin Si
-        }//Fin Si
+            if (iMinus >= (1 / 3) * (iMayus + iMinus) || iMayus >= (1 / 3) * (iMayus + iMinus)) {
+                sMensaje = sMensaje + "- Debe tener al menos 1/3 de los caracteres diferentes(mayus/minus).<br>";
+            } //Fin Si
+        } //Fin Si
 
-    }else{
-        sMensaje="Debe introducir una contrasena de al menos 10 caracteres.";
-    }//Fin Si
+    } else {
+        sMensaje = "Debe introducir una contrasena de al menos 10 caracteres.";
+    } //Fin Si
 
     document.getElementById("resultado").innerHTML = sMensaje;
 } //Fin Funcion
--- Actividades js/P1/script.js
+++ Actividades js/P1/script.js
@@ -2,7 +2,7 @@ function ordenaNum(a, b) {
     if (a[1] === b[1]) {
         return 0;
     } else {
-        return (a[1] < b[1]) ? -1 : 1;
+        return (a[1] > b[1]) ? -1 : 1;
     } //Fin  si
 }
 
